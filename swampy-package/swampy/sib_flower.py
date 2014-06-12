df3d6fc413f88e5eb924746691ca6e65630c7a7d
bgassler <bgassler4@gmail.com>
2014-06-12 15:31:02 -0400

    lab 1

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,22 +1,28 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Brendan Gassler
 
+from sib_polygon import *
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
-bob = Turtle()				
+bob = Turtle()	
+bob.delay = 0
 
 
+def petal(turtle, radius, angle):
+	for i in range(2):
+		arc(turtle, radius, angle)
+		lt(turtle, 180-angle) 
 
-# This is where you put code to move bob
+#petal(bob, 40, 60)
 
+def flower(turtle, n, radius, angle):
+	for i in range(n):
+		petal(turtle, radius, angle)
+		lt(turtle, 360/n)
 
-
-
-
-
-
+flower(bob, 14, 300, 20)
 
 wait_for_user()					
 
