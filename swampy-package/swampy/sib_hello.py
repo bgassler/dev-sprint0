779894dcf564a664298dc16ac5de68aa6e74f2b5
bgassler <bgassler4@gmail.com>
2014-06-12 16:00:09 -0400

    lab1

--- swampy-package/swampy/sib_hello.py
+++ swampy-package/swampy/sib_hello.py
@@ -1,15 +1,88 @@
 # Hello excercise from Week 0
 
-# Name:
+# Name: Brendan Gassler
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
-bob = Turtle()				
+bob = Turtle()
+bob.delay = 0				
+
 
 
 
 # This is where you put code to move bob
+
+def hello(turtle):
+	lt(turtle)
+	#drawing H
+    	fd(turtle, 100)
+        bk(turtle, 50)
+        rt(turtle)
+        fd(turtle, 50) 
+        lt(turtle)
+        fd(turtle, 50)
+        bk(turtle, 100)
+        rt(turtle)
+        pu(turtle)
+        fd(turtle, 25)
+        #start drawing E
+        pd(turtle)
+        fd(turtle, 50)
+        bk(turtle, 50)
+        lt(turtle)
+        fd(turtle, 50)
+        rt(turtle)
+        fd(turtle, 25)
+        bk(turtle, 25)
+        lt(turtle)
+        fd(turtle, 50)
+        rt(turtle)
+        fd(turtle, 50)
+        pu(turtle)
+        fd(turtle, 25)
+        pd(turtle)
+        #drawing first L
+        rt(turtle)
+        fd(turtle, 100)
+        lt(turtle)
+        fd(turtle, 60)
+        pu(turtle)
+        fd(turtle, 25)
+        pd(turtle)
+        #draw second L
+        fd(turtle, 60)
+        bk(turtle, 60)
+        lt(turtle)
+        fd(turtle, 100)
+        rt(turtle)
+        pu(turtle)
+        fd(turtle, 85)
+        pd(turtle)
+        fd(turtle, 50)
+        rt(turtle)
+        fd(turtle, 100)
+        rt(turtle)
+        fd(turtle, 50)
+        rt(turtle)
+        fd(turtle, 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hello(bob)
 
 
 
@@ -19,3 +92,6 @@ bob = Turtle()
 
 
 wait_for_user()					
+
+
+
